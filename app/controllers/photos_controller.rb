4b24aa94260d130c154282601ee8e6d112481fda
Raghu Betina <raghu@firstdraft.com>
2017-03-28 16:07:45 +0000

    Automatically associate comment to signed in user

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -47,8 +47,6 @@ class PhotosController < ApplicationController
   def update
     @photo = Photo.find(params[:id])
 
-    @photo.user_id = params[:user_id]
-
     save_status = @photo.save
 
     if save_status == true
